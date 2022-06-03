7d922fc0f77f3faa1de375e5433491fca959bfcb
blaufish <blaufish@users.noreply.github.com>
2022-06-03 23:55:05 +0200

    Also remove dependencies from cmd

--- secure-copy.cmd
+++ secure-copy.cmd
@@ -1,1 +1,1 @@
-java %JAVA_OPTS% -cp target\classes;target\lib\commons-io-2.10.0.jar;target\classes;target\lib\commons-codec-1.10.jar org.securecopy.SecureCopy %*
+java %JAVA_OPTS% -cp target\classes org.securecopy.SecureCopy %*
